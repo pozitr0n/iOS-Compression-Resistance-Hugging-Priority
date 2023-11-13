b025b2434f2e3ab468a37c465e23e648798d97aa
Raman Kozar <pozitr0n.kozarroman@gmail.com>
2023-11-13 19:23:41 +0100

    Added changes to the test project

---
+++ CompressionResistance-HuggingPriority/CompressionResistance-HuggingPriority/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  CompressionResistance-HuggingPriority
+//
+//  Created by Raman Kozar on 13/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let productLabel = {
+        
+        $0.text = "Product"
+        $0.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        $0.backgroundColor = .green
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+        
+    }(UILabel())
+    
+    let appleMacBookLabel = {
+        
+        $0.text = "Apple MacBook Pro 2022 16' Retina 512GB"
+        $0.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        $0.backgroundColor = .orange
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+        
+    }(UILabel())
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        view.addSubview(productLabel)
+        view.addSubview(appleMacBookLabel)
+        
+        setConstraints()
+        setCompressionResistanceHuggingPriority()
+        
+    }
+    
+    func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+        
+            // Constraints for productLabel
+            productLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 30),
+            productLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 0),
+            
+            // Constraints for appleMacBookLabel
+            appleMacBookLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 30),
+            appleMacBookLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: 0),
+            
+            // Constraints between appleMacBookLabel and productLabel
+            appleMacBookLabel.leadingAnchor.constraint(equalTo: productLabel.trailingAnchor, constant: 0)
+            
+        ])
+        
+    }
+    
+    func setCompressionResistanceHuggingPriority() {
+      
+        //productLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        //appleMacBookLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        
+        productLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        appleMacBookLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        
+    }
+
+}
+
